b6b734fe6daf107b33f0bd54b813bd7c55135890
ShaninS <xeonxayir88@gmail.com>
2025-08-07 12:27:53 +0600

    ann fxml, design || aud fxml, design || user fxml, design updated

--- src/main/java/bmasec2/bmaapplication/shanin/AnnouncementApprovalViewController.java
+++ src/main/java/bmasec2/bmaapplication/shanin/AnnouncementApprovalViewController.java
@@ -1,7 +1,25 @@
 package bmasec2.bmaapplication.shanin;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextArea;
+
 public class AnnouncementApprovalViewController
 {
     @javafx.fxml.FXML
+    private ListView pendingAnnouncementListView;
+    @javafx.fxml.FXML
+    private TextArea announcementContentTextArea;
+
+    @javafx.fxml.FXML
     public void initialize() {
-    }}
+    }
+
+    @javafx.fxml.FXML
+    public void approveBtnOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void rejectBtnOnAction(ActionEvent actionEvent) {
+    }
+}
--- src/main/java/bmasec2/bmaapplication/shanin/UserManagementViewController.java
+++ src/main/java/bmasec2/bmaapplication/shanin/UserManagementViewController.java
@@ -1,7 +1,58 @@
 package bmasec2.bmaapplication.shanin;
 
+import javafx.event.ActionEvent;
+import javafx.event.Event;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+
 public class UserManagementViewController
 {
     @javafx.fxml.FXML
+    private ComboBox filterByRoleComboBox;
+    @javafx.fxml.FXML
+    private TableColumn fullNameColumn;
+    @javafx.fxml.FXML
+    private TextField emailTextField;
+    @javafx.fxml.FXML
+    private ComboBox roleComboBox;
+    @javafx.fxml.FXML
+    private TableColumn userIdColumn;
+    @javafx.fxml.FXML
+    private TableColumn roleColumn;
+    @javafx.fxml.FXML
+    private TextField userIdTextField;
+    @javafx.fxml.FXML
+    private ComboBox statusComboBox;
+    @javafx.fxml.FXML
+    private TableColumn statusColumn;
+    @javafx.fxml.FXML
+    private TextField fullNameTextField;
+    @javafx.fxml.FXML
+    private TextField searchByNameTextfield;
+    @javafx.fxml.FXML
+    private TableView userListTableView;
+    @javafx.fxml.FXML
+    private TableColumn emailColumn;
+
+    @javafx.fxml.FXML
     public void initialize() {
-    }}
+    }
+
+    @javafx.fxml.FXML
+    public void crossBtnOnMouseClicked(Event event) {
+    }
+
+    @javafx.fxml.FXML
+    public void deleteSelectedBtnOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void addNewUserBtnOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void submitBtnOnAction(ActionEvent actionEvent) {
+    }
+}
